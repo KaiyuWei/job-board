cfc0309b7d77fc459009494037e16c7832f97432
KaiyuWei <weikaiyu2020@outlook.com>
2023-12-23 20:38:16 +0100

    job-card completed and applied

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,30 +1,11 @@
 <x-layout>
     @foreach ($jobs as $job)
-        <x-card class="mb-4">
-            <div class="mb-4 flex justify-between">
-                <h2 class="text-lg font-medium">{{ $job->title }}</h2>
-                <div class="text-slate-500">
-                    ${{ number_format($job->salary) }}
-                </div>
-            </div>
-            <div class="mb-4 flex items-center justify-between text-sm text-slate-500">
-                <div class="flex space-x-4">
-                    <div>Company Name</div>
-                    <div>{{ $job->location }}</div>
-                </div>
-                <div class="flex space-x-1 text-xs">
-                    <x-tag>{{ Str::ucfirst($job->experience) }}</x-tag>
-                    <x-tag>{{ $job->category }}</x-tag>
-                </div>
-            </div>
-            <p class="mb-4 text-sm text-slate-500">
-                {!! nl2br(e($job->description)) !!}
-            </p>
+        <x-job-card class="mb-4" :job="$job">
             <div>
                 <x-link-button :href="route('jobs.show', $job)">
                     View
                 </x-link-button>
             </div>
-        </x-card>
+        </x-job-card>
     @endforeach
 </x-layout>
